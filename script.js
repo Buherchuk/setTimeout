2afb7ee5d45fb5c79eb630cdd7dd28519bcc1422
Bohdan Buherchuk <bugerchuk@kpnu.km.ua>
2020-01-04 20:17:21 +0200

    SetInterval function

---
+++ script.js
@@ -0,0 +1,11 @@
+function printNumbers(from, to) {
+    let counter = -1;
+        
+    
+
+    document.getElementById("setTimeout").innerHTML = counter;
+    
+    setTimeout("printNumbers()",1000);
+
+  }
+  
